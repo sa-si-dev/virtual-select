b70c9141a25605aeb008d0d028936cdceefa137c
Gonçalo M. <goncalo.martins.fct@gmail.com>
2025-04-30 15:33:38 +0100

    Entrance animation issue when openDropbox is triggered programatically (#402)
    * Entrance animation issue when openDropbox is triggered programatically
    
    * Update src/virtual-select.js
    
    Co-authored-by: José Rio <jose.rio@hotmail.com>
    
    * Update src/virtual-select.js
    
    Co-authored-by: José Rio <jose.rio@hotmail.com>
    
    * Fix syntax error
    
    ---------
    
    Co-authored-by: José Rio <jose.rio@hotmail.com>

--- src/virtual-select.js
+++ src/virtual-select.js
@@ -2346,6 +2346,14 @@ export class VirtualSelect {
   }
 
   openDropbox(isSilent) {
+    let originalTransition = '';
+    // Disable transitions for programmatic opening
+    if (!isSilent) {
+      // Store original transition
+      originalTransition = this.$dropboxContainer.style.transition;
+      this.$dropboxContainer.style.transition = 'none';
+    }
+    // Perform the open operation
     this.isSilentOpen = isSilent;
 
     DomUtils.setAttr(this.$dropboxWrapper, 'tabindex', '0');
@@ -2365,9 +2373,16 @@ export class VirtualSelect {
     }
 
     this.setDropboxWrapperWidth();
-
     DomUtils.removeClass(this.$allWrappers, 'closed');
     DomUtils.changeTabIndex(this.$allWrappers, 0);
+
+    if (!isSilent) {
+      // Force synchronous layout and style calculation
+      // Trigger reflow
+      this.$dropboxContainer.offsetHeight; // eslint-disable-line no-unused-expressions
+      // Restore transitions immediately after reflow
+      this.$dropboxContainer.style.transition = originalTransition;
+    }
 
     if (this.dropboxPopover && !isSilent) {
       this.dropboxPopover.show();
