78bce1146ffaa001c8ef3af3937288b6c95b469e
gnbm <goncalo.martins@outsystems.com>
2026-06-08 13:37:15 +0100

    Revert "Potential fix for pull request finding"
    This reverts commit 98c964986a685f5878b9c60e80cddb4bb01b51da.

--- src/virtual-select.js
+++ src/virtual-select.js
@@ -3778,7 +3778,7 @@ export class VirtualSelect {
    * `onResizeMethod` reference, which a throttled anonymous wrapper would otherwise lose).
    * @type {ReturnType<typeof Utils.throttle> | undefined}
    */
-  // onResizeThrottled is assigned after class definition (see listener registration below).
+  static onResizeThrottled;
 
   static onResizeMethod() {
     document.querySelectorAll('.vscomp-ele-wrapper').forEach(($ele) => {
